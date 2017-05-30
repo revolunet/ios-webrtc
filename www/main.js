f0fba5e37c795dd684d2abdc116af7a898574371
Julien Bouquillon <julien@revolunet.com>
2017-05-30 03:05:27 +0200

    cosmetics

--- www/main.js
+++ www/main.js
@@ -6,46 +6,33 @@
  *  tree.
  */
 
-'use strict';
+"use strict"
 
-var errorElement = document.querySelector('#errorMsg');
-var video = document.querySelector('video');
+var errorElement = document.querySelector("#errorMsg")
+var video = document.querySelector("video")
 
 // Put variables in global scope to make them available to the browser console.
-var constraints = window.constraints = {
+var constraints = (window.constraints = {
   audio: false,
   video: true
-};
+})
 
 function handleSuccess(stream) {
-  var videoTracks = stream.getVideoTracks();
-  console.log('Got stream with constraints:', constraints);
-  console.log('Using video device: ' + videoTracks[0].label);
+  //var videoTracks = stream.getVideoTracks()
   stream.oninactive = function() {
-    console.log('Stream inactive');
-  };
-  window.stream = stream; // make variable available to browser console
-  video.srcObject = stream;
+    console.log("Stream inactive")
+  }
+  video.src = URL.createObjectURL(stream);
 }
 
 function handleError(error) {
-  if (error.name === 'ConstraintNotSatisfiedError') {
-    errorMsg('The resolution ' + constraints.video.width.exact + 'x' +
-        constraints.video.width.exact + ' px is not supported by your device.');
-  } else if (error.name === 'PermissionDeniedError') {
-    errorMsg('Permissions have not been granted to use your camera and ' +
-      'microphone, you need to allow the page access to your devices in ' +
-      'order for the demo to work.');
-  }
-  errorMsg('getUserMedia error: ' + error.name, error);
+  console.error("getUserMedia error: " + error.name, error)
 }
 
-function errorMsg(msg, error) {
-  errorElement.innerHTML += '<p>' + msg + '</p>';
-  if (typeof error !== 'undefined') {
-    console.error(error);
-  }
-}
+var toggleCount = 0;
 
-navigator.mediaDevices.getUserMedia(constraints).
-    then(handleSuccess).catch(handleError);
+function toggle() {
+  var targetDevice = "com.apple.avfoundation.avcapturedevice.built-in_video:" + (toggleCount % 2 ? 1 : 0);
+  navigator.mediaDevices.getUserMedia(Object.assign({}, constraints, { video: { deviceId: targetDevice } })).then(handleSuccess).catch(handleError);
+  toggleCount++;
+}
